9de18c171fa0d6c5414ecd308ad2c9ac26dd422b
PAWAN KUMAR <pawangiri3@gmail.com>
2025-12-29 22:58:25 +0530

    schript change

--- scripts/install_python.sh
+++ scripts/install_python.sh
@@ -23,9 +23,9 @@ sudo npm install pm2 -g
 echo "[INFO] Preparing backend setup variables"
 
 # Accept DB server and DB name as positional args or from environment variables
-DB_SERVER="${1:-${DB_SERVER:-devopsinssrv1.database.windows.net}}"
+DB_SERVER="${1:-${DB_SERVER:-todoappserversrv1.database.windows.net}}"
 DB_NAME="${2:-${DB_NAME:-todoappdb}}"
-DB_USER="${DB_USER:-devopsadmin}"
+DB_USER="${DB_USER:-sqladmin}"
 DB_PASS="${DB_PASS:-P@ssw01rd@123}"
 
 echo "[INFO] Using DB Server: $DB_SERVER"
@@ -33,12 +33,12 @@ echo "[INFO] Using DB Name: $DB_NAME"
 
 echo "[INFO] Setting up Backend Application for First Time"
 
-sudo -u devopsadmin env DB_SERVER="$DB_SERVER" DB_NAME="$DB_NAME" DB_USER="$DB_USER" DB_PASS="$DB_PASS" bash -lc '
-cd /home/devopsadmin/
+sudo -u sqladmin env DB_SERVER="$DB_SERVER" DB_NAME="$DB_NAME" DB_USER="$DB_USER" DB_PASS="$DB_PASS" bash -lc '
+cd /home/sqladmin/
 if [ ! -d todoapp-backend-py ]; then
 	git clone https://github.com/devopsinsiders/todoapp-backend-py.git
 fi
-cd /home/devopsadmin/todoapp-backend-py
+cd /home/sqladmin/todoapp-backend-py
 cat > .env <<EOF
 CONNECTION_STRING="Driver={ODBC Driver 18 for SQL Server};Server=tcp:$DB_SERVER,1433;Database=$DB_NAME;Uid=$DB_USER;Pwd=$DB_PASS;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
 EOF
